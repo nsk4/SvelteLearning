4cdbdc2462523a7c412689d6f4c79ced9e2ec3d8
Nejc Smrkolj <nejc.smrkolj@cosylab.com>
2024-01-26 13:22:17 +0900

    Finished animations and transitions section

--- src/lib/TodoList.svelte
+++ src/lib/TodoList.svelte
@@ -2,12 +2,26 @@
     import Button from './Button.svelte';
     import { createEventDispatcher, afterUpdate } from 'svelte';
     import IoIosClose from 'svelte-icons/io/IoIosClose.svelte';
+    import { flip } from 'svelte/animate';
+    import { scale, crossfade } from 'svelte/transition';
+
+    const [sendTransition, receiveTransition] = crossfade({
+        duration: 300,
+        fallback(node) {
+            return scale(node, { start: 0.5, duration: 300 });
+        }
+    });
 
     export let todos = null;
     export let error = null;
     export let isLoading = false;
-    export let disableAdding = false;
     export let disabledItems = [];
+    export let disableAdding = false;
+    export let scrollOnAdd = undefined;
+
+    $: doneList = todos ? todos.filter((t) => t.completed) : [];
+    $: todoList = todos ? todos.filter((t) => !t.completed) : [];
+
     let prevTodos = todos;
     let inputText = '';
     let input, listDiv, listDivScrollHeight;
@@ -20,9 +34,17 @@
     }
 
     afterUpdate(() => {
-        if (autoScroll) {
-            listDiv.scrollTo(0, listDivScrollHeight);
-            autoScroll = false;
+        if (scrollOnAdd) {
+            let pos;
+            if (scrollOnAdd === 'top') {
+                pos = 0;
+            } else if (scrollOnAdd === 'bottom') {
+                pos = listDivScrollHeight;
+            }
+            if (autoScroll) {
+                listDiv.scrollTo({ top: pos, behavior: 'smooth' });
+                autoScroll = false;
+            }
         }
     });
 
@@ -58,34 +80,55 @@
                 {#if todos.length === 0}
                     <p class="state-text">No todos yet</p>
                 {:else}
-                    <ul>
-                        {#each todos as { id, title, completed } (id)}
-                            <li class:completed>
-                                <label>
-                                    <input
-                                        disabled={disabledItems.includes(id)}
-                                        on:input={(event) => {
-                                            event.currentTarget.checked = completed;
-                                            handleToggleTodo(id, !completed);
-                                        }}
-                                        type="checkbox"
-                                        checked={completed}
-                                    />
-                                    {title}
-                                </label>
-                                <button
-                                    disabled={disabledItems.includes(id)}
-                                    class="remove-todo-button"
-                                    aria-label="Remove todo: {title}"
-                                    on:click={() => handleremoveTodo(id)}
-                                >
-                                    <span style:width="25px" style:display="inline-block"
-                                        ><IoIosClose /></span
-                                    >
-                                </button>
-                            </li>
+                    <div style:display="flex">
+                        {#each [todoList, doneList] as list, index}
+                            <div class="list-wrapper">
+                                <h2>{index === 0 ? 'Todo' : 'Done'}</h2>
+                                <ul>
+                                    {#each list as todo (todo.id)}
+                                        {@const { id, title, completed } = todo}
+                                        <li animate:flip={{ duration: 300 }}>
+                                            <slot {todo}>
+                                                <div
+                                                    in:receiveTransition|local={{ key: id }}
+                                                    out:sendTransition|local={{ key: id }}
+                                                    class:completed
+                                                >
+                                                    <label>
+                                                        <input
+                                                            disabled={disabledItems.includes(id)}
+                                                            on:input={(event) => {
+                                                                event.currentTarget.checked =
+                                                                    completed;
+                                                                handleToggleTodo(id, !completed);
+                                                            }}
+                                                            type="checkbox"
+                                                            checked={completed}
+                                                        />
+                                                        <slot name="title">
+                                                            {title}
+                                                        </slot>
+                                                    </label>
+                                                    <button
+                                                        disabled={disabledItems.includes(id)}
+                                                        class="remove-todo-button"
+                                                        aria-label="Remove todo: {title}"
+                                                        on:click={() => handleremoveTodo(id)}
+                                                    >
+                                                        <span
+                                                            style:width="25px"
+                                                            style:display="inline-block"
+                                                            ><IoIosClose /></span
+                                                        >
+                                                    </button>
+                                                </div>
+                                            </slot>
+                                        </li>
+                                    {/each}
+                                </ul>
+                            </div>
                         {/each}
-                    </ul>
+                    </div>
                 {/if}
             </div>
         </div>
@@ -112,54 +155,61 @@
         }
 
         .todo-list {
-            max-height: 200px;
+            max-height: 400px;
             overflow: auto;
-            ul {
-                margin: 0;
+            .list-wrapper {
                 padding: 10px;
-                list-style: none;
-                li {
-                    margin-bottom: 5px;
-                    display: flex;
-                    align-items: center;
-                    background-color: #303030;
-                    border-radius: 5px;
-                    padding: 10px;
-                    position: relative;
-                    label {
-                        cursor: pointer;
-                        font-size: 18px;
+                flex: 1;
+                h2 {
+                    margin: 0 0 10px;
+                }
+                ul {
+                    margin: 0;
+                    padding: 0;
+                    list-style: none;
+                    li > div {
+                        margin-bottom: 5px;
                         display: flex;
-                        align-items: baseline;
-                        padding-right: 25px;
-                        input[type='checkbox'] {
-                            margin: 0 10px 0 0;
+                        align-items: center;
+                        background-color: #303030;
+                        border-radius: 5px;
+                        padding: 10px;
+                        position: relative;
+                        label {
                             cursor: pointer;
+                            font-size: 18px;
+                            display: flex;
+                            align-items: baseline;
+                            padding-right: 25px;
+                            input[type='checkbox'] {
+                                margin: 0 10px 0 0;
+                                cursor: pointer;
+                            }
                         }
-                    }
 
-                    &.completed > label {
-                        opacity: 0.5;
-                        text-decoration: line-through;
-                    }
-                    .remove-todo-button {
-                        border: none;
-                        background: none;
-                        padding: 5px;
-                        position: absolute;
-                        right: 5px;
-                        cursor: pointer;
-                        &:disabled {
-                            opacity: 0.4;
-                            cursor: not-allowed;
+                        &.completed > label {
+                            opacity: 0.5;
+                            text-decoration: line-through;
                         }
-                        :global(svg) {
-                            fill: #bd1414;
-                        }
-                    }
-                    &:hover {
                         .remove-todo-button {
-                            display: block;
+                            border: none;
+                            background: none;
+                            padding: 5px;
+                            position: absolute;
+                            right: 5px;
+                            cursor: pointer;
+                            &:disabled {
+                                opacity: 0.4;
+                                cursor: not-allowed;
+                            }
+                            :global(svg) {
+                                fill: #bd1414;
+                            }
+                        }
+                        &:hover {
+                            .remove-todo-button {
+                                display: block;
+                            }
                         }
                     }
                 }
